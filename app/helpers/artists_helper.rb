08b4a1028f671578861b6e60ddc565ab42b0e850
derrick b <dbozich1@gmail.com>
2018-08-29 02:47:04 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,7 +3,7 @@ module ArtistsHelper
     if song.artist.present?
       link_to song.artist.name, artist_path(song.artist)
     else
-      
+      link_to "Edit Page", edit_song_path(song)
     end
   end
 end
